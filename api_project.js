7b57032d47a78db6fe541c8f0da85a29e3e51f8b
Alli Abdulateef <alli5723@gmail.com>
2018-03-18 00:35:39 +0200

    Added documentation for Authentication header /event/planner/{id}

--- api_project.js
+++ api_project.js
@@ -1,1 +1,1 @@
-define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-03-16T08:01:37.005Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
+define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-03-17T22:34:05.075Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
